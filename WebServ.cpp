e71c5c2a6f9ba6bf62b3119c9057f2a89dc48c00
Youngchan Kim <youngcki@cx1r2s2.42seoul.kr>
2024-04-02 20:17:33 +0900

    just modified WebServ.cpp

--- WebServ.cpp
+++ WebServ.cpp
@@ -41,16 +41,9 @@ void WebServ::RunServer()
 		int nev = kevent(m_kq, NULL, 0, ev_list, 50, NULL);
 		for (int i = 0; i < nev; i++)
 		{
-			// if (ev_list[i].filter == EVFILT_READ && ev_list[i].flags & EV_EOF) // 연결 종료
-			// {
-			// 	close(ev_list[i].ident);
-			// 	std::cout << "connection closed" << std::endl;
-			// 	continue;
-			// }
-			// else 
 			if (ev_list[i].filter == EVFILT_READ) // read 이벤트
 			{
-				if (std::find(m_servSocks.begin(), m_servSocks.end(), ev_list[i].ident) != m_servSocks.end()) // 서버 소켓 - 새로운 연결
+				if (m_document.GetFdEvent().find(ev_list[i].ident) != m_document.GetFdEvent().end() && m_document.GetFdEvent().find(ev_list[i].ident)->second == "server")
 				{
 					struct sockaddr_in clnt_adr;
 					socklen_t adr_sz = sizeof(clnt_adr);
@@ -63,16 +56,16 @@ void WebServ::RunServer()
 					m_document.PutFdEvent(clntSock, "client");
 					std::cout << "new connection" << std::endl;
 				}
-				else if (m_document.GetExcute().find(ev_list[i].ident) != m_document.GetExcute().end()) // cgi 소켓 - 응답 처리
+				else
 				{
-					// std::cout << "read event for cgi" << std::endl;
-					// m_cgiProcessor.Read(m_document, ev_list[i].ident);
+					if (m_document.GetExcute().find(ev_list[i].ident) != m_document.GetExcute().end()) // cgi 소켓 - 응답 처리
+					{
+						// std::cout << "read event for cgi" << std::endl;
+						// m_cgiProcessor.Read(m_document, ev_list[i].ident);
+					}
+					else // 클라이언트 소켓 - 요청 처리
+						m_requestMaker.makeRequest(m_document, ev_list[i].ident);
 				}
-				else // 클라이언트 소켓 - 요청 처리
-				{
-					m_requestMaker.makeRequest(m_document, ev_list[i].ident);
-				}
-				
 			}
 			else if (ev_list[i].filter == EVFILT_WRITE) // write 이벤트
 			{
@@ -90,29 +83,9 @@ void WebServ::RunServer()
 		m_classifier.Classify(m_document);
 		m_staticProcessor.Process(m_document);
 		m_document.ClearDynamic();
-		// std::cout << "static" << std::endl;
-		// std::vector<Request>& requests_s = m_document.GetStatic();
-		// for (std::vector<Request>::iterator it = requests_s.begin(); it != requests_s.end(); it++)
-		// {
-		// 	it->PrintRequest();
-		// }
-		// std::vector<Request>& requests_d = m_document.GetDynamic();
-
-		// std::cout << "dynamic" << std::endl;
-		// for (std::vector<Request>::iterator it = requests_d.begin(); it != requests_d.end(); it++)
-		// {
-		// 	it->PrintRequest();
-		// }
 		// m_document.ClearStatic();
-		// m_document.ClearDynamic();
-		// vector 받아서 실행 -> 실행상태 반환
-		// m_requestProcessor.ProcessRequests(m_document);
-		// m_cgiProcessor.ExcuteCgi(m_document);
-		//wait 해야함
-		// response 보내기
+		m_cgiProcessor.ExcuteCgi(m_document);
 	}
-
-	
 }
 
 int WebServ::openPort(int port)
